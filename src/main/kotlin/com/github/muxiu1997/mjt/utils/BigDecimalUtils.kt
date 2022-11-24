cfe0eb4637e8fbdde9be19234feef3b449efb2a8
MuXiu1997 <muxiu1997@gmail.com>
2022-11-24 14:02:59 +0800

    Update BigDecimalUtils

--- src/main/kotlin/com/github/muxiu1997/mjt/utils/BigDecimalUtils.kt
+++ src/main/kotlin/com/github/muxiu1997/mjt/utils/BigDecimalUtils.kt
@@ -5,6 +5,9 @@ import java.math.BigDecimal
 import java.math.RoundingMode
 
 object BigDecimalUtils {
+    /**
+     * Convert an object to [BigDecimal], return [BigDecimal.ZERO] if the object is null or cannot be converted to [BigDecimal]
+     */
     @Contract(pure = true)
     private fun Any?.toBigDecimalOrZero(): BigDecimal = try {
         when (this) {
@@ -16,40 +19,133 @@ object BigDecimalUtils {
         BigDecimal.ZERO
     }
 
+    /**
+     * Convert an object to [BigDecimal], return [BigDecimal.ZERO] if the object is null or cannot be converted to [BigDecimal]
+     * @see toBigDecimalOrZero
+     */
     @JvmStatic
     @Contract(pure = true)
-    fun getBigDecimalOrZero(value: Any?): BigDecimal = value.toBigDecimalOrZero()
+    fun getBigDecimalOrZero(`object`: Any?): BigDecimal = `object`.toBigDecimalOrZero()
 
+    /**
+     * Returns a [BigDecimal] whose value is (a + b)
+     *
+     * objects that cannot be converted to [BigDecimal] will be converted to [BigDecimal.ZERO].
+     * @see toBigDecimalOrZero
+     */
     @JvmStatic
     @Contract(pure = true)
-    fun safeAdd(a: Any?, b: Any?): BigDecimal = a.toBigDecimalOrZero() + b.toBigDecimalOrZero()
+    infix fun Any?.safeAdd(other: Any?): BigDecimal = this.toBigDecimalOrZero() + other.toBigDecimalOrZero()
 
+    /**
+     * Returns a [BigDecimal] whose value is (a - b)
+     *
+     * objects that cannot be converted to [BigDecimal] will be converted to [BigDecimal.ZERO].
+     * @see toBigDecimalOrZero
+     */
     @JvmStatic
     @Contract(pure = true)
-    fun safeSub(a: Any?, b: Any?): BigDecimal = a.toBigDecimalOrZero() - b.toBigDecimalOrZero()
+    infix fun Any?.safeSub(other: Any?): BigDecimal = this.toBigDecimalOrZero() - other.toBigDecimalOrZero()
 
+    /**
+     * Returns a [BigDecimal] whose value is (a * b)
+     *
+     * objects that cannot be converted to [BigDecimal] will be converted to [BigDecimal.ZERO].
+     * @see toBigDecimalOrZero
+     */
     @JvmStatic
     @Contract(pure = true)
-    fun safeMul(a: Any?, b: Any?): BigDecimal = a.toBigDecimalOrZero() * b.toBigDecimalOrZero()
+    infix fun Any?.safeMul(other: Any?): BigDecimal = this.toBigDecimalOrZero() * other.toBigDecimalOrZero()
 
+    /**
+     * Returns a [BigDecimal] whose value is (a / b)
+     *
+     * objects that cannot be converted to [BigDecimal] will be converted to [BigDecimal.ZERO].
+     * @see toBigDecimalOrZero
+     */
+    @JvmName("_safeDiv")
     @JvmStatic
     @Contract(pure = true)
-    fun safeDiv(a: Any?, b: Any?, scale: Int, roundingMode: RoundingMode): BigDecimal = try {
+    infix fun Any?.safeDiv(other: Any?): BigDecimal = safeDiv(this, other)
+
+    /**
+     * Returns a [BigDecimal] whose value is (a / b)
+     *
+     * objects that cannot be converted to [BigDecimal] will be converted to [BigDecimal.ZERO].
+     * @see toBigDecimalOrZero
+     */
+    @JvmStatic
+    @JvmOverloads
+    @Contract(pure = true)
+    fun safeDiv(a: Any?, b: Any?, scale: Int = 2, roundingMode: RoundingMode = RoundingMode.HALF_UP): BigDecimal = try {
         a.toBigDecimalOrZero().divide(b.toBigDecimalOrZero(), scale, roundingMode)
     } catch (ignored: Exception) {
         BigDecimal.ZERO.setScale(scale)
     }
 
+    /**
+     * Returns a [BigDecimal] whose value is the sum of the objects
+     *
+     * objects that cannot be converted to [BigDecimal] will be converted to [BigDecimal.ZERO].
+     * @see toBigDecimalOrZero
+     * @see safeAdd
+     */
     @JvmStatic
     @Contract(pure = true)
-    fun safeDiv(a: Any?, b: Any?, scale: Int): BigDecimal = safeDiv(a, b, scale, RoundingMode.HALF_UP)
+    fun safeSum(vararg objects: Any?): BigDecimal = objects.fold(BigDecimal.ZERO) { a, b -> a safeAdd b }
 
+    /**
+     * Returns a [BigDecimal] whose value is the sum of the objects in the collection
+     *
+     * objects that cannot be converted to [BigDecimal] will be converted to [BigDecimal.ZERO].
+     * @see toBigDecimalOrZero
+     * @see safeAdd
+     */
     @JvmStatic
     @Contract(pure = true)
-    fun safeDiv(a: Any?, b: Any?): BigDecimal = safeDiv(a, b, 2)
+    fun <T> Iterable<T>?.safeSum(): BigDecimal {
+        return this?.fold(BigDecimal.ZERO) { a, b -> a safeAdd b } ?: BigDecimal.ZERO
+    }
 
+    /**
+     * Returns a [BigDecimal] whose value is the sum of the objects in the sequence
+     *
+     * objects that cannot be converted to [BigDecimal] will be converted to [BigDecimal.ZERO].
+     * @see toBigDecimalOrZero
+     * @see safeAdd
+     */
     @JvmStatic
     @Contract(pure = true)
-    fun safeSum(vararg nums: Any?): BigDecimal = nums.map { it.toBigDecimalOrZero() }.reduce(this::safeAdd)
+    fun <T> Sequence<T>?.safeSum(): BigDecimal {
+        return this?.fold(BigDecimal.ZERO) { a, b -> a safeAdd b } ?: BigDecimal.ZERO
+    }
+
+
+    /**
+     * Returns a [BigDecimal] whose value is sum of all values produced by [selector] function applied to each element in the collection
+     *
+     * objects that cannot be converted to [BigDecimal] will be converted to [BigDecimal.ZERO]
+     * @see toBigDecimalOrZero
+     * @see safeAdd
+     */
+    @JvmStatic
+    @Contract(pure = true)
+    fun <T> Iterable<T>?.safeSumOf(selector: (T) -> Any?): BigDecimal {
+        return this?.map(selector)?.fold(BigDecimal.ZERO) { a, b -> a safeAdd b } ?: BigDecimal.ZERO
+    }
+
+    /**
+     * Returns a [BigDecimal] whose value is sum of all values produced by [selector] function applied to each element in the sequence
+     *
+     * objects that cannot be converted to [BigDecimal] will be converted to [BigDecimal.ZERO]
+     * @see toBigDecimalOrZero
+     * @see safeAdd
+     */
+    @JvmStatic
+    @Contract(pure = true)
+    fun <T> Sequence<T>?.safeSumOf(selector: (T) -> Any?): BigDecimal {
+        1..2
+        return this?.map(selector)?.fold(BigDecimal.ZERO) { a, b -> a safeAdd b } ?: BigDecimal.ZERO
+    }
 }
 
--- src/test/kotlin/com/github/muxiu1997/mjt/utils/BigDecimalUtilsTest.kt
+++ src/test/kotlin/com/github/muxiu1997/mjt/utils/BigDecimalUtilsTest.kt
@@ -1,5 +1,11 @@
 package com.github.muxiu1997.mjt.utils
 
+import com.github.muxiu1997.mjt.utils.BigDecimalUtils.safeAdd
+import com.github.muxiu1997.mjt.utils.BigDecimalUtils.safeDiv
+import com.github.muxiu1997.mjt.utils.BigDecimalUtils.safeMul
+import com.github.muxiu1997.mjt.utils.BigDecimalUtils.safeSub
+import com.github.muxiu1997.mjt.utils.BigDecimalUtils.safeSum
+import com.github.muxiu1997.mjt.utils.BigDecimalUtils.safeSumOf
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.math.BigDecimal
@@ -29,59 +35,88 @@ internal class BigDecimalUtilsTest {
 
     @Test
     fun safeAdd() {
-        assertEquals(BigDecimal("5"), BigDecimalUtils.safeAdd("3", "2"))
-        assertEquals(BigDecimal("3"), BigDecimalUtils.safeAdd("3", null))
-        assertEquals(BigDecimal("2"), BigDecimalUtils.safeAdd(null, "2"))
-        assertEquals(BigDecimal.ZERO, BigDecimalUtils.safeAdd(null, null))
+        assertEquals(BigDecimal("5"), "3" safeAdd "2")
+        assertEquals(BigDecimal("3"), "3" safeAdd null)
+        assertEquals(BigDecimal("2"), null safeAdd "2")
+        assertEquals(BigDecimal.ZERO, null safeAdd null)
     }
 
     @Test
     fun safeSub() {
-        assertEquals(BigDecimal("1"), BigDecimalUtils.safeSub("3", "2"))
-        assertEquals(BigDecimal("3"), BigDecimalUtils.safeSub("3", null))
-        assertEquals(BigDecimal("-2"), BigDecimalUtils.safeSub(null, "2"))
-        assertEquals(BigDecimal.ZERO, BigDecimalUtils.safeSub(null, null))
+        assertEquals(BigDecimal("1"), "3" safeSub "2")
+        assertEquals(BigDecimal("3"), "3" safeSub null)
+        assertEquals(BigDecimal("-2"), null safeSub "2")
+        assertEquals(BigDecimal.ZERO, null safeSub null)
     }
 
     @Test
     fun safeMul() {
-        assertEquals(BigDecimal("6"), BigDecimalUtils.safeMul("3", "2"))
-        assertEquals(BigDecimal.ZERO, BigDecimalUtils.safeMul("3", null))
-        assertEquals(BigDecimal.ZERO, BigDecimalUtils.safeMul(null, "2"))
-        assertEquals(BigDecimal.ZERO, BigDecimalUtils.safeMul(null, null))
+        assertEquals(BigDecimal("6"), "3" safeMul "2")
+        assertEquals(BigDecimal.ZERO, "3" safeMul null)
+        assertEquals(BigDecimal.ZERO, null safeMul "2")
+        assertEquals(BigDecimal.ZERO, null safeMul null)
     }
 
     @Test
     fun safeDiv() {
-        assertEquals(BigDecimal("1.50"), BigDecimalUtils.safeDiv("3", "2"))
-        assertEquals(BigDecimal("0.00"), BigDecimalUtils.safeDiv("3", null))
-        assertEquals(BigDecimal("0.00"), BigDecimalUtils.safeDiv(null, "2"))
-        assertEquals(BigDecimal("0.00"), BigDecimalUtils.safeDiv(null, null))
-        assertEquals(BigDecimal("0.00"), BigDecimalUtils.safeDiv("3", "0.00"))
+        assertEquals(BigDecimal("1.50"), "3" safeDiv "2")
+        assertEquals(BigDecimal("0.00"), "3" safeDiv null)
+        assertEquals(BigDecimal("0.00"), null safeDiv "2")
+        assertEquals(BigDecimal("0.00"), null safeDiv null)
+        assertEquals(BigDecimal("0.00"), "3" safeDiv "0.00")
     }
 
     @Test
     fun safeDivWithScale() {
-        assertEquals(BigDecimal("1.5000"), BigDecimalUtils.safeDiv("3", "2", 4))
-        assertEquals(BigDecimal("0.0000"), BigDecimalUtils.safeDiv("3", null, 4))
-        assertEquals(BigDecimal("0.0000"), BigDecimalUtils.safeDiv(null, "2", 4))
-        assertEquals(BigDecimal("0.0000"), BigDecimalUtils.safeDiv(null, null, 4))
-        assertEquals(BigDecimal("0.0000"), BigDecimalUtils.safeDiv("3", "0.00", 4))
+        assertEquals(BigDecimal("1.5000"), safeDiv("3", "2", 4))
+        assertEquals(BigDecimal("0.0000"), safeDiv("3", null, 4))
+        assertEquals(BigDecimal("0.0000"), safeDiv(null, "2", 4))
+        assertEquals(BigDecimal("0.0000"), safeDiv(null, null, 4))
+        assertEquals(BigDecimal("0.0000"), safeDiv("3", "0.00", 4))
     }
 
     @Test
     fun safeDivWithScaleAndRoundingMode() {
-        assertEquals(BigDecimal("1"), BigDecimalUtils.safeDiv(BigDecimal("3"), BigDecimal("2"), 0, RoundingMode.DOWN))
-        assertEquals(BigDecimal("0"), BigDecimalUtils.safeDiv(BigDecimal("3"), null, 0, RoundingMode.DOWN))
-        assertEquals(BigDecimal("0"), BigDecimalUtils.safeDiv(null, BigDecimal("2"), 0, RoundingMode.DOWN))
-        assertEquals(BigDecimal("0"), BigDecimalUtils.safeDiv(null, null, 0, RoundingMode.DOWN))
-        assertEquals(BigDecimal("0"), BigDecimalUtils.safeDiv("3", "0.00", 0, RoundingMode.DOWN))
+        assertEquals(BigDecimal("1"), safeDiv("3", "2", 0, RoundingMode.DOWN))
+        assertEquals(BigDecimal("0"), safeDiv("3", null, 0, RoundingMode.DOWN))
+        assertEquals(BigDecimal("0"), safeDiv(null, "2", 0, RoundingMode.DOWN))
+        assertEquals(BigDecimal("0"), safeDiv(null, null, 0, RoundingMode.DOWN))
+        assertEquals(BigDecimal("0"), safeDiv("3", "0.00", 0, RoundingMode.DOWN))
     }
 
     @Test
-    fun safeSum() {
-        assertEquals(BigDecimal("6"), BigDecimalUtils.safeSum("1", "2", "3"))
-        assertEquals(BigDecimal("3"), BigDecimalUtils.safeSum("1", "2", null))
-        assertEquals(BigDecimal("0"), BigDecimalUtils.safeSum(null, null, null))
+    fun safeSum_Array() {
+        assertEquals(BigDecimal("6"), safeSum("1", "2", "3"))
+        assertEquals(BigDecimal("3"), safeSum("1", "2", null))
+        assertEquals(BigDecimal("0"), safeSum(null, null, null))
+    }
+
+    @Test
+    fun safeSum_Iterable() {
+        assertEquals(BigDecimal("6"), listOf("1", "2", "3").safeSum())
+        assertEquals(BigDecimal("3"), listOf("1", "2", null).safeSum())
+        assertEquals(BigDecimal("0"), listOf(null, null, null).safeSum())
+    }
+
+    @Test
+    fun safeSum_Sequence() {
+        assertEquals(BigDecimal("6"), listOf("1", "2", "3").asSequence().safeSum())
+        assertEquals(BigDecimal("3"), listOf("1", "2", null).asSequence().safeSum())
+        assertEquals(BigDecimal("0"), listOf(null, null, null).asSequence().safeSum())
+    }
+
+
+    @Test
+    fun safeSumOf_Iterable() {
+        assertEquals(BigDecimal("6"), listOf("1", "2", "3").safeSumOf { it })
+        assertEquals(BigDecimal("3"), listOf("1", "2", null).safeSumOf { it })
+        assertEquals(BigDecimal("0"), listOf(null, null, null).safeSumOf { it })
+    }
+
+    @Test
+    fun safeSumOf_Sequence() {
+        assertEquals(BigDecimal("6"), listOf("1", "2", "3").asSequence().safeSumOf { it })
+        assertEquals(BigDecimal("3"), listOf("1", "2", null).asSequence().safeSumOf { it })
+        assertEquals(BigDecimal("0"), listOf(null, null, null).asSequence().safeSumOf { it })
     }
 }
